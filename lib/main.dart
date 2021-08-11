5218c852b8d46b42dea1d668dcdd9e3bbcd917a8
viswamvs <viswavickymvs@gmail.com>
2021-08-11 19:32:37 +0530

    added scroll view feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import 'package:personal_expense_tracker/widgets/new_transaction.dart';
-import 'package:personal_expense_tracker/widgets/transaction_list.dart';
-import 'package:personal_expense_tracker/widgets/user_transaction.dart';
+import './widgets/user_transaction.dart';
 import './models/transaction.dart';
 
 void main() {
@@ -21,9 +19,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,19 +26,27 @@ class MyHomePage extends StatelessWidget {
         title: Text('Personal Expense Tracker'),
         backgroundColor: Colors.purple,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.cyan,
-              child: Text('CHART!'),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.cyan,
+                child: Text('CHART!'),
+              ),
             ),
-          ),
-          UserTransaction()
-        ],
+            UserTransaction()
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
